4069f39c5285800c799ba491ac33410b42c301c0
DESKTOP-HDN491L\tom kentenich <tomkentenich18@gmail.com>
2022-02-22 18:03:40 +0530

    few changes

---
+++ main(without_tkinter).py
@@ -0,0 +1,131 @@
+import re
+
+
+def login():
+    print("WELCOME TO LOGIN PAGE")
+
+    file = open("user.txt", "r")
+    maills = []
+    passls = []
+    for i in file:
+        a,b = i.split(",")
+        b = b.strip()
+        maills.append(a)
+        passls.append(b)
+    data = dict(zip(maills, passls))
+    file.close()
+
+    email = input("enter your mail: ")
+
+    if email not in maills:
+        print("userid not found \n")
+        print("no mail id found please register ")
+        print("type 'login' to go to login page")
+        print("or type 'register' to go to register page\n\n")
+        while True:
+            lorr = input()
+            if lorr == 'login':
+                login()
+            elif lorr == "register":
+                register()
+            else:
+                print("choose between 'login' or 'register'")
+
+    elif email in maills:
+        password = input("enter your password: ")
+        if password!=data[email]:
+            print("wrong password")
+            print("type 'forget' to know your password")
+            print("or type 'signup' to go to signup page\n\n")
+
+            while True:
+                forget = input()
+                if forget == 'forget':
+                    print(f"your password for the {email} id is {data[email]}")
+                if forget == "signup":
+                    signup()
+                else:
+                    print("choose between 'forget' or 'signup'")
+        else:
+            print("welcome to website page")
+            exit(0)
+
+
+
+def mail(email,maills):
+    mail_val = "^[a-z]+[\._]?[a-z 0-9]+[@]\w+[.]\w{2,3}$"
+    if re.search(mail_val, email):
+        if email in maills:
+            print("mail id already exist")
+            register()
+        else:
+            password(email)
+    else:
+        print("please enter valid mailid")
+        register()
+
+
+def password(email):
+    password = input("enter your password: ")
+    password2 = input("enter your password again: ")
+    if password == password2:
+        if len(password) <= 5:
+            print("password is too short")
+            register()
+        elif len(password) > 15:
+            print("password is too long")
+            register()
+        elif not re.search("[a-z]", password):
+            print("password must have atleast one lower-case")
+            register()
+        elif not re.search("[A-Z]", password):
+            print("password must have atleast one upper")
+            register()
+        elif not re.search("[0-9]", password):
+            print("password must have atleast one number")
+            register()
+        elif not re.search("[$&+,:;=?@#|'<>.-^*()%!]", password):
+            print("password must have atleast one special character")
+            register()
+        else:
+            file = open("user.txt", "a")
+            file.write(email + "," + password+"\n")
+            file.close()
+            print("account registered,please sign in\n\n")
+            login()
+    else:
+        print("passwords are not matching")
+        register()
+
+def register():
+    print("WELCOME TO REGISTER PAGE")
+
+    file = open("user.txt", "r")
+    maills = []
+    passls = []
+    for i in file:
+        a,b = i.split(",")
+        b = b.strip()
+        maills.append(a)
+        passls.append(b)
+
+    data = dict(zip(maills,passls))
+    file.close()
+
+    email = input("enter your mail: ")
+    mail(email,maills)
+
+
+def signup():
+    print("welcome to login page \n")
+    choose = input("login or signup: ")
+    if choose == "login":
+        login()
+    elif choose == "signup":
+        register()
+    else:
+        print("please enter a valid input\n\n")
+        signup()
+
+signup()
+
---
+++ main.py
@@ -0,0 +1,145 @@
+from tkinter import *
+import re
+
+def welcome():
+    welcome=Tk()
+    welcome.geometry("300x250")
+    label1 = Label(welcome, text="welcome to the home page you are log in successfully").pack()
+    button2 = Button(welcome, text="sign up", command=signup).pack()
+
+
+
+def forgetpass():
+    li=[]
+    li.append(data[str(email2.get())])
+    label1 = Label(Login, text=li)
+    label1.pack()
+    button1 = Button(Login, text="sign up", command=signup)
+    button1.pack()
+def malvar2():
+    if email2.get() not in maills:
+        label22 = Label(Login,text="userid not found \n")
+        label22.pack()
+
+
+    elif email2.get() in maills:
+        button2 = Button(Login, text="forget password", command=forgetpass)
+        button2.pack()
+        if str(password.get())!=data[email2.get()]:
+            label3 = Label(Login,text="wrong passsword \n").pack()
+        else:
+            welcome()
+
+
+
+def login():
+    global Login
+    Login=Tk()
+    Login.geometry("300x250")
+
+    file = open("user.txt", "r")
+    global maills
+    maills = []
+    passls = []
+    for i in file:
+        a, b = i.split(",")
+        b = b.strip()
+        maills.append(a)
+        passls.append(b)
+    global data
+    data = dict(zip(maills, passls))
+    print(data)
+    file.close()
+
+
+    global email2
+    global password
+
+    label1=Label(Login,text="login").pack()
+    label2 = Label(Login, text="username").pack()
+    email2 = Entry(Login)
+    email2.pack()
+    label3 = Label(Login, text="password").pack()
+    password = Entry(Login)
+    password.pack()
+    button1 = Button(Login, text="login", command=malvar2)
+    button1.pack()
+
+
+
+def mailver():
+    mail_val = "^[a-z]+[\._]?[a-z 0-9]+[@]\w+[.]\w{2,3}$"
+    if re.search(mail_val, email.get()):
+        if email in maills:
+            label1 = Label(register, text="mail already exists").pack()
+        else:
+            if password.get() == password2.get():
+                if len(password.get()) <= 5:
+                    label1 = Label(register, text="password is too short").pack()
+                elif len(password.get()) > 15:
+                    label1 = Label(register, text="password is too long").pack()
+                elif not re.search("[a-z]", password.get()):
+                    label1 = Label(register, text="password must have atleast one lower-case").pack()
+                elif not re.search("[A-Z]", password.get()):
+                    label1 = Label(register, text="password must have atleast one upper").pack()
+                elif not re.search("[0-9]", password.get()):
+                    label1 = Label(register, text="password must have atleast one number").pack()
+                elif not re.search("[$&+,:;=?@#|'<>.-^*()%!]", password.get()):
+                    label1 = Label(register, text="password must have atleast one special character").pack()
+                else:
+                    file = open("user.txt", "a")
+                    file.write(str(email.get())+","+str(password.get())+"\n")
+                    file.close()
+                    # print("account registered,please sign in\n\n")
+                    login()
+            else:
+                label1 = Label(register, text="passwords are not matching")
+                label1.pack()
+    else:
+        label1 = Label(register, text="please enter valid mailid")
+        label1.pack()
+
+def register():
+    file = open("user.txt", "r")
+    global maills
+    maills = []
+    passls = []
+    for i in file:
+        a, b = i.split(",")
+        b = b.strip()
+        maills.append(a)
+        passls.append(b)
+    data = dict(zip(maills, passls))
+    print(data)
+    file.close()
+    global register
+    register = Tk()
+    register.geometry("300x250")
+    global email
+    global password
+    global password2
+    label1 = Label(register, text="register").pack()
+    label2 = Label(register, text="username").pack()
+    email=Entry(register)
+    email.pack()
+    label3 = Label(register, text="password").pack()
+    password = Entry(register)
+    password.pack()
+    label4 = Label(register, text="confirm password").pack()
+    password2 = Entry(register)
+    password2.pack()
+    button1 = Button(register, text="register", command=mailver)
+    button1.pack()
+
+
+def signup():
+    signup = Tk()
+    signup.geometry("300x250")
+    label1=Label(signup,text="welcome to home page").pack()
+    button1=Button(signup,text="login",command=login).pack()
+    button2=Button(signup,text="register",command=register).pack()
+    button3=Button(signup,text="exit",command=quit).pack()
+    signup.mainloop()
+
+signup()
+
